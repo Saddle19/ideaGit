18b88c3512d681e9f709c9c1415afb94de199695
saddle <saddleshiJD@168.com>
2021-05-30 18:10:30 +0800

    向本地仓库中添加一个testGit

---
+++ git-test-project/src/gitTry/testGit.java
@@ -0,0 +1,12 @@
+package gitTry;
+
+public class testGit {
+    public static void main(String[] args) {
+        System.out.println("jiba");
+        System.out.println("jiba");
+        System.out.println("jiba");
+        System.out.println("jiba");
+        System.out.println("jiba");
+        System.out.println("jiba");
+    }
+}
